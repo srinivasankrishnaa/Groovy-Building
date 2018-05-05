2e20777641ef85b79888f89ce089543a93ef76f6
srinivasankrishnaa <srinivasankrishnaa@gmail.com>
2018-05-05 18:10:01 +0530

    Xml Insert Node And Replace Methods Added

--- script/Gkxml.groovy
+++ script/Gkxml.groovy
@@ -1,16 +1,22 @@
 package  com.bhanu.master.xml
 import groovy.xml.*
 import com.bhanu.master.excel.Gexcel;
-import com.bhanu.master.file.Gfileprocess;
+import com.bhanu.master.file.Gfileprocess
+import org.ietf.jgss.GSSException;
 
 class Gxmlprocess {
     def obj1 = new Gfileprocess()
+    def exobj=new Gexcel()
     def rdr
 
     def loadxmlfile(String filename) {
         //xmlfile=new File(filename)
         obj1.loadfile(filename)
         rdr = new XmlSlurper().parse(obj1.rfile)
+    }
+    def xmltxtparse(String txt)
+    {
+        rdr = new XmlSlurper().parseText(txt)
     }
 
     def readxmlfile() { obj1.rfile.eachLine { println it } }
@@ -19,6 +25,7 @@ class Gxmlprocess {
 
     def xmlserialize(Boolean print, Boolean savefile)
     {
+        //xmlserialize(false,true)
         def out = XmlUtil.serialize(rdr)
 
         if (print == true) {println out}      //Serialized Output On scree
@@ -36,13 +43,25 @@ class Gxmlprocess {
 
     }
 
-    def xmlinsert(String path,String ref,String upd) {
-
-        root=rdr
+    def xmlinsert(String path,String refvar,String refval,String updtrvar,String updtrval) {
+    //xmlinsert("make","Company","Mercedes","Owner","Bhanuchander")
+        def root=rdr
         def outputBuilder = new StreamingMarkupBuilder()
-        root."$path".find {(it.@"$ref" == 'Mercedes')}.@"$upd" = 'Bhanuchander'
+       root."$path".find {(it.@"$refvar" == refval)}.@"$updtrvar" = updtrval
+        //rdr.make.find {(it.@Company == 'Mercedes')}.@Added = 'Bhanuchander'
         String result = outputBuilder.bind { mkp.yield root }
-        println result
+        xmltxtparse(result)
+        return result
+    }
+    def xmlrep(String path,String refvar,String refval,String updtrvar,String updtrval) {
+        //xmlinsert("make","Company","Mercedes","Owner","Bhanuchander")
+        def root=rdr
+        def outputBuilder = new StreamingMarkupBuilder()
+        root."$path".findAll {(it.@"$refvar" == refval)}."$updtrvar" = updtrval
+        //rdr.make.find {(it.@Company == 'Mercedes')}.@Added = 'Bhanuchander'
+        String result = outputBuilder.bind { mkp.yield root }
+        xmltxtparse(result)
+        return result
     }
 
 }
